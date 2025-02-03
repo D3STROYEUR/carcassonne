4452f2e89072bb93a6b36271eab21490df408e07
Noahrng <araneganoah@gmail.com>
2025-02-03 10:16:30 +0100

    ./test/test_affichage.c

--- test/test_affichage.c
+++ test/test_affichage.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "affichage.h"
+#include "preparation.h"
+#include "gestion.h"
 
+
+int main(){
+    char t[5] = {'v','r','r','v','r'};
+    struct Tuile *tuile = creerTuile(t);
+    struct Meeple *meeple = creerMeeple(0,'r');
+
+    tuile->meeple = meeple;
+    afficherTuile(*tuile);
+
+    return 0;
+}
